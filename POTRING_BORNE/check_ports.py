8cf99aed62bd3aeb6a86a0f382a77da4ab26cd3e
samuel- <samuel.braikeh@yahoo.fr>
2017-06-18 01:26:34 +0200

    scan des ports serie (arduinos)

--- POTRING_BORNE/check_ports.py
+++ POTRING_BORNE/check_ports.py
@@ -11,10 +11,13 @@
 
 
 import serial
+from time import sleep
+
 if __name__ == '__main__':
-        cmd = str(raw_input('type s/S to scan ports 0-10 or a specific port number to test it. '))
+        cmd = str(raw_input('type : \n s/S to scan ports 0-10 \n c/C to configure ports for Potring \n'))
         #print cmd
         #print len(cmd)
+        
         if len(cmd) == 1 and ('s' in cmd or 'S' in cmd):
                 print "start scanning ports"
                 for num in range(11):
@@ -22,7 +25,46 @@ if __name__ == '__main__':
                         try:
                                 ser = serial.Serial(port, 9600, timeout=0)
                                 print (port+"\t OK \n")
+                                sleep(0.1)
+                                ser.close()
                         except:
                                 print (port+"\t -- \n")
                                 pass
-
+                raw_input("entree pour quitter")
+        if len(cmd) == 1 and ('c' in cmd or 'C' in cmd):
+                raw_input("debrancher les 2 cartes arduino svp")
+                sleep(0.5)
+                p0=''
+                p1=''
+                raw_input("brancher uniquement l'arduino du capteur de gauche svp")
+                sleep(0.5)
+                for num in range(11):
+                        port = 'COM'+str(num)
+                        try:
+                                ser0 = serial.Serial(port, 9600, timeout=0)
+                                print (port+"\t OK \n")
+                                p0=port
+                                break
+                        except:
+                                pass
+                raw_input("brancher le deuxieme arduino en plus svp")
+                sleep(0.5)
+                for num in range(11):
+                        port = 'COM'+str(num)
+                        try:
+                                ser1 = serial.Serial(port, 9600, timeout=0)
+                                print (port+"\t OK \n")
+                                p1=port
+                                break
+                        except:
+                                pass
+                ser0.close()
+                ser1.close()
+                if p0!='' and p1!='':
+                        outline = "p0: "+p0+"\n"+"p1: "+p1
+                        print outline
+                        with open("ports.config", "w") as file:
+                                file.write(outline)
+                else:
+                        print "erreur"
+                raw_input("entree pour quitter")
--- POTRING_BORNE/recherche_arduino.py
+++ POTRING_BORNE/recherche_arduino.py
@@ -23,45 +23,68 @@ def chercher(MAIN=False):
     chercher_arduino(MAIN)
     #chercher_clavier(MAIN)
 ###############
+tags=['4CA684','4CAB53','4CAB3E']
     
 ##############################
 ##############################
 def chercher_arduino(MAIN):
     try:
         #ser = serial.Serial('COM5', 9600, timeout=1, parity='N')
-        ser0 = serial.Serial('COM5', 9600, timeout=0)
+        with open("ports.config", "r") as file:
+            port0 = 'COM'+str(int(file.readline().split('COM', 1)[1]))
+            port1 = 'COM'+str(int(file.readline().split('COM', 1)[1]))
+            print port0
+            print port1
     except:
-        try:
-            ser0 = serial.Serial('COM6', 9600, timeout=0)
-        except:
-            print ("\n erreur.\n essayer de debrancher - rebrancher la carte arduino\n")
-            #sys.exit()
-            j.jeu_go = False
-        
+        print ("\n pas de fichier de config des ports... lancer check_ports.py svp\n")
+        j.jeu_go = False
+    try:
+        ser0 = serial.Serial(port0, 9600, timeout=0)
+        ser1 = serial.Serial(port1, 9600, timeout=0)
+    except:
+        print ("\n probleme avec les cartes arduino. les rebrancher ou relancer check_ports.py\n")
+        j.jeu_go = False        
 
     print ("rfid okay")
     sleep(1)
     while j.jeu_go:
-	try:
+        try:
             line = ser0.readline()
             if line != "": print line
-            if ('4D4B7E' in line):
+            if ('VOID' in line):
                 j.remplir(0,-1)
-		j.remplir(1,-1)
-		j.remplir(0,0)
-		j.remplir(1,1)
-	    if ('VOID' in line):
-                j.remplir(0,-1)
-		j.remplir(1,-1)
-            if ('4CA86F' in line):
-		j.remplir(0,-1)
-		j.remplir(1,-1)
-		j.remplir(0,1)
-		j.remplir(1,2)
-	except ser0.timeout:
+            elif (tags[0] in line):
+                j.remplir(0,0)
+            elif (tags[1] in line):
+                j.remplir(0,1)
+            elif (tags[2] in line):
+                j.remplir(0,2)
+        except ser0.timeout:
             print('Data could not be read')
-            sleep(0.1)
+        try:
+            line = ser1.readline()
+            if line != "": print line
+            if ('VOID' in line):
+                j.remplir(1,-1)
+            elif (tags[0] in line):
+                j.remplir(1,0)
+            elif (tags[1] in line):
+                j.remplir(1,1)
+            elif (tags[2] in line):
+                j.remplir(1,2)
+        except ser1.timeout:
+            print('Data could not be read')
 ####################
 if __name__ == '__main__':
     j.jeu_go = True
+    j.reset_ring()
     chercher(MAIN=True)
+    if j.jeu_go==False:
+        try:
+            ser0.close()
+        except:
+            pass
+        try:
+            ser1.close()
+        except:
+            pass
